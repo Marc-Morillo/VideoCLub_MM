d89151dbdf1392c526ea8ebfedca7395b5764a92
Marc-Morillo <86896794+Marc-Morillo@users.noreply.github.com>
2022-04-23 11:15:20 -0400

    Iniciando pagina web y login

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiVideoClub.Models;
+
+namespace MiVideoClub.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      public ActionResult Enter(string user, string password)
+      {
+          try
+          {
+            using (VideoClubEntities db = new VideoClubEntities())
+            {
+              var ls = from d in db.Empleados
+                       where d.NombreUsuario == user && d.Contrasena == password && d.idEstado == 1
+                       select d;
+              if(ls.Count() > 0)
+              {
+                Empleados oUser = ls.First();
+                Session["User"] = oUser;
+                return Content("1");
+              }
+              else
+              {
+                return Content("Usuario Invalido :(");
+              }
+            }    
+          }
+          catch(Exception ex)
+          {
+            return Content("Ocurrio un error :( " + ex.Message);
+          }
+      }
+    }
+}
---
+++ Filters/VerifySession.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiVideoClub.Models;
+using MiVideoClub.Controllers;
+
+namespace MiVideoClub.Filters
+{
+  public class VerifySession : ActionFilterAttribute
+  {
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+      var oUser = (Empleados)HttpContext.Current.Session["User"];
+      if(oUser == null)
+      {
+        if(filterContext.Controller is AccessController == false)
+        {
+          filterContext.HttpContext.Response.Redirect("Access/Index");
+        }
+      }
+
+      base.OnActionExecuting(filterContext);
+    }
+  }
+}
